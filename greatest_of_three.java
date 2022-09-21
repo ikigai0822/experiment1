983eb578abdaa96c617d5bc2bedc3fe2c56cf5fa
ikigai0822 <110655746+ikigai0822@users.noreply.github.com>
2022-09-21 11:01:41 +0530

    Add files via upload

---
+++ greatest_of_three.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class greatest_of_three {
+	public static void main(String[] args) 
+	{
+	
+		int a, b, c ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the three numbers");	
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		if(a>b && a>c)
+			System.out.println("a is the greatest\n" +  a);
+		else if(b>a && b>c)
+			System.out.println("b is the greatest\n"+ b);
+		else if (c>b && c>a)
+			System.out.println("c is the greatest\n"+c);
+    }
+}
